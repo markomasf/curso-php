35ef89fb554b92bb9498b50420731280f4762c49
Marco <markomasf@gmail.com>
2025-01-21 20:20:31 -0300

    aula de php relembrando

---
+++ screen-match.php
@@ -0,0 +1,9 @@
+<?php
+echo "Bem-vindo(a) ao Screen Match!\n";
+
+$nomeFilme = "Top Gun - Maverick";
+$anoLancamento = 2022;
+$notaFilme = 8.8;
+$incluidoNoPlano = true;
+
+echo "Nome do filme: " . $nomeFilme;
