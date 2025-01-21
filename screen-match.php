c12ac9b7619cd4637f4c4709ff9559a26a46ebce
Marco <markomasf@gmail.com>
2025-01-21 20:37:41 -0300

    entrada de dados

--- screen-match.php
+++ screen-match.php
@@ -2,8 +2,18 @@
 echo "Bem-vindo(a) ao Screen Match!\n";
 
 $nomeFilme = "Top Gun - Maverick";
-$anoLancamento = 2022;
-$notaFilme = 8.8;
-$incluidoNoPlano = true;
+$anoLancamento = $argv[1] ?? 2022;
+$somaNotas = 9;
+$somaNotas += 6;
+$somaNotas += 8;
+$somaNotas += 7.5;
+$somaNotas += 5;
 
-echo "Nome do filme: " . $nomeFilme;
+$notaFilme = $somaNotas/5;
+$planoPrime = true;
+
+$incluidoNoPlano = $planoPrime || $anoLancamento > 2020;
+
+echo "Nome do filme: " . $nomeFilme . "\n";
+echo "Média da Nota: " . $notaFilme. "\n";
+echo "Ano de Lançamento: " . $anoLancamento . "\n";
